5e28974dac2cecd4e1125760b8b99dcdf61c8219
Shihao Liu <lsh20011018@gmail.com>
2023-01-11 02:04:31 -0700

    Step 10 completed

--- lab1.py
+++ lab1.py
@@ -1,6 +1,14 @@
 import requests
+from pathlib import Path
 
 print(requests.__version__)
 
 print(requests.get("http://google.com").text)
 
+url = "https://raw.githubusercontent.com/MichaelLsh/cmput404-lab1/main/lab1.py"
+r  = requests.get(url)
+
+p = Path(__file__).with_name('lab01.py')
+with p.open('w') as f:
+    f.write(r.text)
+print(r.text)
